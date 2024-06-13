5a0a4892b1b61bda5de372460ec440c9da48409e
shpark <sh0913.park@samsung.com>
2024-06-13 16:34:17 +0900

    feat & refactoring: add check validation

--- similarity_checker.py
+++ similarity_checker.py
@@ -1,2 +1,11 @@
 class SimilarityChecker:
-    pass
+    def check_valid(self, str1, str2):
+        if not isinstance(str1, str) or not isinstance(str2, str):
+            raise TypeError
+        if not str1.isupper() or not str2.isupper():
+            raise TypeError
+
+    def calc(self, str1, str2):
+        score = 0
+        self.check_valid(str1, str2)
+        return score
--- test_similarity_checker.py
+++ test_similarity_checker.py
@@ -1,5 +1,22 @@
 from unittest import TestCase
 
+from similarity_checker import SimilarityChecker
+
 
 class TestSimilarityChecker(TestCase):
-    pass
+    def setUp(self):
+        self.similarity_checker = SimilarityChecker()
+
+    def assert_not_exception(self, str1, str2):
+        try:
+            self.similarity_checker.calc(str1, str2)
+            self.fail()
+        except TypeError:
+            pass
+
+    def test_exception_when_input_is_none(self):
+        self.assert_not_exception('ABC', None)
+        self.assert_not_exception(None, 'ABC')
+        self.assert_not_exception('123', 'ABC')
+        self.assert_not_exception('abc', 'ABC')
+        self.assert_not_exception('', 'ABC')
