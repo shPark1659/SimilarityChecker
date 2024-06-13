2c0dcf10f329eb8fc5c06f64a79bf163431a493e
shpark <sh0913.park@samsung.com>
2024-06-13 15:52:34 +0900

    feat: raise exception when inuput is none

--- similarity_checker.py
+++ similarity_checker.py
@@ -1,2 +1,5 @@
 class SimilarityChecker:
-    pass
+    def check(self, str1, str2):
+        if not isinstance(str1, str) or not isinstance(str2, str):
+            raise TypeError
+
--- test_similarity_checker.py
+++ test_similarity_checker.py
@@ -1,5 +1,12 @@
 from unittest import TestCase
 
+from similarity_checker import SimilarityChecker
+
 
 class TestSimilarityChecker(TestCase):
-    pass
+    def setUp(self):
+        self.similarity_checker = SimilarityChecker()
+
+    def test_exception_when_input_is_none(self):
+        with self.assertRaises(TypeError):
+            self.similarity_checker.check(None, 'ABC')
