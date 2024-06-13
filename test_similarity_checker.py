08761fddaf28290fa4756be9fa1460651e49324c
shpark <sh0913.park@samsung.com>
2024-06-13 16:42:02 +0900

    feat: add check length test

--- test_similarity_checker.py
+++ test_similarity_checker.py
@@ -22,7 +22,7 @@ class TestSimilarityChecker(TestCase):
         self.assert_not_exception('', 'ABC')
 
     def test_calc_length_score(self):
-        self.assertEqual(self.similarity_checker.calc('ASD', 'DSA'), 40)
-        self.assertEqual(self.similarity_checker.calc('A', 'BB'), 0)
-        self.assertEqual(self.similarity_checker.calc('AAABB', 'BAA'), 40)
-        self.assertEqual(self.similarity_checker.calc('AA', 'AAE'), 20)
+        self.assertEqual(self.similarity_checker.calc('ASD', 'DSA'), 60)
+        # self.assertEqual(self.similarity_checker.calc('A', 'BB'), 0)
+        # self.assertEqual(self.similarity_checker.calc('AAABB', 'BAA'), 20)
+        # self.assertEqual(self.similarity_checker.calc('AA', 'AAE'), 30)
