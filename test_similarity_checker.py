050e912b41274ca1846ccfa7bfe628cc6040db60
shpark <sh0913.park@samsung.com>
2024-06-13 15:57:12 +0900

    refactoring: organize exception tests

--- test_similarity_checker.py
+++ test_similarity_checker.py
@@ -7,6 +7,13 @@ class TestSimilarityChecker(TestCase):
     def setUp(self):
         self.similarity_checker = SimilarityChecker()
 
+    def assert_not_exception(self, str1, str2):
+        try:
+            self.similarity_checker.check(str1, str2)
+            self.fail()
+        except TypeError:
+            pass
+
     def test_exception_when_input_is_none(self):
-        with self.assertRaises(TypeError):
-            self.similarity_checker.check(None, 'ABC')
+        self.assert_not_exception('ABC', None)
+        self.assert_not_exception(None, 'ABC')
